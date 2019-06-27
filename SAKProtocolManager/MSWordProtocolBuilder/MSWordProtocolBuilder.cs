0203c3050fde8da96588140c32b6e634aaa15f58
Roman Kozvonin <romankozvonin@gmail.com>
2019-06-27 16:43:30 +0400

    Доработка отрисовки AoAz

--- SAKProtocolManager/MSWordProtocolBuilder/MSWordProtocolBuilder.cs
+++ SAKProtocolManager/MSWordProtocolBuilder/MSWordProtocolBuilder.cs
@@ -114,7 +114,7 @@ namespace SAKProtocolManager.MSWordProtocolBuilder
                         int colsAmount = (endRecEl - startRecEl+1)*structure.BendingTypeLeadsNumber/2;
                         int rowsAll = rList.ElementsCount + 6;
                         Debug.WriteLine($"add_AoAz_Table: table = {tableNum} rowsAll = {rowsAll} colsAmount = {colsAmount}, startRec = {startRecEl}, endRec = {endRecEl}");
-                        int[] tableRows = CalcMaxRowsCount(colsAmount, rowsAll, 5);
+                        int[] tableRows = CalcMaxRowsCount_For_AoAz(colsAmount, rowsAll);
                         for(int rIdx = 0; rIdx < tableRows.Length; rIdx++)
                         {
                             int rowsAmount = tableRows[rIdx];
@@ -1064,6 +1064,14 @@ namespace SAKProtocolManager.MSWordProtocolBuilder
             return table;
         }
 
+        private static int[] CalcMaxRowsCount_For_AoAz(int cols, int rows)
+        {
+            SubTable[] subTables = wordProtocol.EstimateTablePosition_For_AoAz(cols, rows);
+            List<int> template = new List<int>();
+            foreach (SubTable st in subTables) template.Add(st.RowsCount);
+            return template.ToArray();
+        }
+
         private static int[] CalcMaxRowsCount(int cols, int rows, int minRowsPerTable = 5)
         {
             SubTable[] subTables = wordProtocol.EstimateTablePosition(cols, rows, minRowsPerTable);
@@ -1427,20 +1435,20 @@ namespace SAKProtocolManager.MSWordProtocolBuilder
 
         public SubTable[] EstimateTablePosition_For_AoAz(int colsCount, int rowsCount)
         {
-            float tableWidth = colsCount * CellWidth;
-            if (tableWidth > PageWidth) tableWidth = PageWidth;
-            int tablesOnPageRow = (int)(PageWidth / tableWidth);
-            int MaxRowsPerTable = 65;
+            float tableWidth = colsCount * CellWidth; // ширина таблицы
+            if (tableWidth > PageWidth) tableWidth = PageWidth; //если таблица выходит за границы её ширина равна ширине страницы
+            int tablesOnPageRow = (int)(PageWidth / tableWidth); //количество помещающихся таблиц в одну строку
+            int MaxRowsPerTable = 50;//65; //макс количество строк на одну таблицу
             //if (MinRowsPerTable > rowsCount) MinRowsPerTable = rowsCount;
-            ShapeCoord lastCoord = LastShapeCoords == null ? new ShapeCoord() { x = 0, y = 0, width = 0, height = 0, page = 0 } :  ;
-            float[] pageLine = (float[])PageLine.Clone();
-            List<SubTable> subTables = new List<SubTable>();
-            int curPage = lastCoord.page;
+            ShapeCoord lastCoord = LastShapeCoords == null ? new ShapeCoord() { x = 0, y = 0, width = 0, height = 0, page = 0 } : LastShapeCoords; //Координаты последней добавленной фигуры
+            float[] pageLine = (float[])PageLine.Clone(); //создаём копию массива фигур для данной строки
+            List<SubTable> subTables = new List<SubTable>(); 
+            int curPage = lastCoord.page; //устанавливаем текущей странице страницу из координат последней фигуры в документе
 
-            float xCoord = (int)lastCoord.x + (int)lastCoord.width;
-            if (xCoord + tableWidth > PageWidth || lastCoord.height < MinRowsPerTable * CellHeight) xCoord = 0f;
-            float line = GetLine(pageLine, (int)lastCoord.x, (int)tableWidth);
-            if (line + MinRowsPerTable * CellHeight > PageHeight)
+            float xCoord = (int)lastCoord.x + (int)lastCoord.width; //устанавливаем текущую координату 
+            if (xCoord + tableWidth > PageWidth) xCoord = 0f; //если фигура не умещается устанавливаем x = 0
+            float line = GetLine(pageLine, (int)lastCoord.x, (int)tableWidth); // ищем строку для вставки
+            if (line + (rowsCount/3) * CellHeight > PageHeight ) // высота 1/3 ячеек таблицы не помещается на текущей странице - переходим на новый лист
             {
                 for (int ps = 0; ps < pageLine.Length; ps++) pageLine[ps] = 0f;
                 xCoord = 0f;
@@ -1449,56 +1457,36 @@ namespace SAKProtocolManager.MSWordProtocolBuilder
 
             while (rowsCount > 0)
             { 
-
-                int rowsOnCurrentPosition; //Количество строк в данной строке документа
-                int colsOnCurrentPosition;
-                int tablesToAddCount = 0;
+                line = GetLine(pageLine, (int)lastCoord.x, (int)tableWidth);
+                int rowsOnCurrentPosition = (int)((PageHeight - line) / CellHeight); //Количество строк в данной строке документа
+                int colsOnCurrentPosition = colsCount; //Количество столбцов в данной строке документа
+                int tablesToAddCount = 0; //количестов таблиц для добавления в документ
                 if (xCoord == 0)
                 {
                     rowsOnCurrentPosition = (int)((PageHeight - line) / CellHeight);
-                    if (rowsOnCurrentPosition > MaxRowsPerTable) rowsOnCurrentPosition = MaxRowsPerTable;
-                    colsOnCurrentPosition = (int)((PageWidth) / CellWidth);
-                    if (rowsOnCurrentPosition * tablesOnPageRow > rowsCount)
+                    tablesToAddCount = tablesOnPageRow;
+                    if (rowsCount / tablesOnPageRow < rowsOnCurrentPosition)
                     {
-                        tablesToAddCount = tablesOnPageRow;
-                        for (int tCnt = 1; tCnt <= tablesOnPageRow; tCnt++)
+                        if (rowsCount > 14)
                         {
-                            if (rowsCount / tCnt > MinRowsPerTable)
-                            {
-                                tablesToAddCount = tCnt;
-                            }
+                            rowsOnCurrentPosition = rowsCount / tablesOnPageRow;
+                        }else
+                        {
+                            rowsOnCurrentPosition = rowsCount;
                         }
-                        rowsOnCurrentPosition = rowsCount / tablesToAddCount;
                     }
-                    else
-                    {
-                        tablesToAddCount = tablesOnPageRow;
-                    }
-
-                }
-                else
+                }else
                 {
-                    //Это выполняется только если таблица вставляется в строку с другим блоком и только для первой таблицы в коллекции
-                    rowsOnCurrentPosition = (int)(lastCoord.height / CellHeight);
-                    colsOnCurrentPosition = (int)((PageWidth - xCoord) / CellWidth);
-                    if (rowsOnCurrentPosition >= rowsCount)
-                    {
-                        rowsOnCurrentPosition = rowsCount;
-                        tablesToAddCount = 1;
-                    }
-                    else
-                    {
-                        tablesToAddCount = colsOnCurrentPosition / colsCount;
-                    }
+                    tablesToAddCount = 1;
                 }
                 for (int tIdx = 0; tIdx < tablesToAddCount; tIdx++)
                 {
                     int rowsToAddCount = rowsCount - rowsOnCurrentPosition < 0 ? rowsCount : rowsOnCurrentPosition;
-                    if (tIdx == tablesToAddCount - 1 && (rowsCount - rowsToAddCount <= MinRowsPerTable)) rowsToAddCount = rowsCount;
+                    if (tIdx == tablesToAddCount - 1 && (rowsCount - rowsToAddCount <= rowsOnCurrentPosition)) rowsToAddCount = rowsCount;
 
                     float tableHeight = rowsToAddCount * CellHeight;
                     ShapeCoord curTableCoord = GetNextShapeCoord(tableWidth, tableHeight, lastCoord, pageLine);
-                    subTables.Add(new SubTable() { TableShapePlanedCoord = curTableCoord, ColumnsCount = colsCount, RowsCount = rowsToAddCount });
+                    subTables.Add(new SubTable() { TableShapePlanedCoord = curTableCoord, ColumnsCount = colsOnCurrentPosition, RowsCount = rowsToAddCount });
                     rowsCount -= rowsToAddCount;
                     lastCoord = curTableCoord;
                 }
